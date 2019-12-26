e67eb6c2597cb109d44d7db665c3ca694b172657
obusorezekiel <obusorezekiel@gmail.com>
2019-12-27 00:22:03 +0100

    All we have done

---
+++ tests/header.test.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+
+let browser, page;
+
+
+beforeEach( async () => {
+    browser = await puppeteer.launch({
+        headless: false
+    });
+    page = await browser.newPage();
+    await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+    await browser.close();
+});
+
+test('we can launch a browser', async () => {
+    //const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+    //expect(text).toEqual('Blogster');
+})
+
+
